cedf1e5c993a5775705ab8e6f86edab940d8f5e7
mariam <makokirvalidze@gmail.com>
2024-06-17 23:06:08 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import profilePic from './279788184_2564307133699575_6612088671312385060_n.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={profilePic} alt="Profile" className="profile-pic" />
+        <h1>About me</h1>
+        <h1>Gallery</h1>
+        <h1>Contact</h1>
       </header>
+      <body className='App-body'>
+      <img src={profilePic} alt="Profile" className="portfolio" />
+      <div className='body-text'>
+      <h1>Hi! I am Mako</h1>
+      <p>Welcome to my page!</p>
+      </div>
+      
+      
+      </body>
     </div>
   );
 }
